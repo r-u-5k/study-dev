54739a8571070e713772b04ab571d98b475d9790
장유진 <yj999900@gmail.com>
2024-04-09 17:53:33 +0900

    .

---
+++ server/src/main/java/hello/container/MyContainerInitV1.java
@@ -0,0 +1,20 @@
+package hello.container;
+
+import java.util.Set;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+public class MyContainerInitV1 implements ServletContainerInitializer {
+
+	@Override
+	public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+		System.out.println("MyContainerInitV1.onStartup");
+		System.out.println("c: " + c);
+		System.out.println("ctx: " + ctx);
+	}
+	
+	
+
+}
