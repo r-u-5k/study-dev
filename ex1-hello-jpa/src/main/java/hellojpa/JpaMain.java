3a4d2c410a83698306274b2cf2570f2e14b5d046
Jin <yj999900@gmail.com>
2024-05-10 16:55:00 +0900

    .

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,25 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+            tx.commit();
+        } catch (Exception e) {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
