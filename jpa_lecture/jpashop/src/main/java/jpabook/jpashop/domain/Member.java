bee0151d2d61a13920c182e3f708c88f22ac79a8
Jin <yj999900@gmail.com>
2024-07-26 21:45:09 +0900

    .

--- jpa_lecture/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpa_lecture/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,12 +19,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
-
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
