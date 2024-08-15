3d7b2c209a799c91f357d90af5ccca67ed4f8a31
Yujin <yj999900@gmail.com>
2024-08-15 18:02:44 +0900

    .

--- django_lecture/my_site/my_app/urls.py
+++ django_lecture/my_site/my_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.simple_view) # domain.com/my_app
+    path("", views.simple_view), 
+    path("variable/", views.variable_view)
 ]
--- django_lecture/my_site/my_app/views.py
+++ django_lecture/my_site/my_app/views.py
@@ -2,6 +2,18 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+
 # Create your views here.
 def simple_view(request):
-    return render(request,'my_app/example.html') # .html
+    # my_app/templates/my_app/example.html
+    return render(request, "my_app/example.html")
+
+
+def variable_view(request):
+    my_var = {
+        "first_name": "RosaLind",
+        "last_name": "franKlin",
+        "some_list": [1, 2, 3],
+        "some_dict": {"inside_key": "inside_value"},
+    }
+    return render(request, "my_app/variable.html", context=my_var)
