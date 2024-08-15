4f612e63a9ffd8109df4dd1b613dd09032fe6e6a
Yujin <yj999900@gmail.com>
2024-08-15 14:52:09 +0900

    .

--- django_lecture/my_site/my_app/views.py
+++ django_lecture/my_site/my_app/views.py
@@ -1,23 +1,7 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 
 # Create your views here.
-
-articles = {
-    "sports": "Sports Page",
-    "finance": "Finance Page",
-    "politics": "Politics Page"
-}
-
-
-def news_view(request, topic):
-    try:
-        return HttpResponse(articles[topic])
-    except:
-        raise Http404("404 GENERIC ERROR")
-
-
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys())
-    topic = topics_list[num_page]
-    return HttpResponseRedirect(reverse('news_view', args=[topic]))
+def simple_view(request):
+    return render(request,'my_app/example.html') # .html
