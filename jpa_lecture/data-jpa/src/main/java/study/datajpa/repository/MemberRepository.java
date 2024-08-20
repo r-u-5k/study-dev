0c58443bfbacf99a4f5041753171901e016a958c
Yujin <yj999900@gmail.com>
2024-08-20 20:42:07 +0900

    .

--- jpa_lecture/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ jpa_lecture/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,24 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
 }
