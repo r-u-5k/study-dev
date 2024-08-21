fc5860015acdaa9c9087fc2a8c104420a0ad8911
Yujin <yj999900@gmail.com>
2024-08-21 15:10:31 +0900

    .

--- jpa_lecture/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ jpa_lecture/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,7 +2,7 @@ package study.datajpa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -26,4 +26,18 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByNames(@Param("names") List<String> names);
 
     Page<Member> findByAge(int age, PageRequest pageRequest);
+
+    //Method Override로 Fetch Join
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    //JPQL + EntityGraph로 Fetch Join
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    //메서드 이름 쿼리에서 특히 편리함
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findByUsernameByEntityGraph(String username);
 }
