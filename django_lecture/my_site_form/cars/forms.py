f4d2e361d42e9516dde47888427526393b3def02
Yujin <yj999900@gmail.com>
2024-08-28 20:32:29 +0900

    .

--- django_lecture/my_site_form/cars/forms.py
+++ django_lecture/my_site_form/cars/forms.py
@@ -1,7 +1,32 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label="First Name",max_length=100)
-    last_name = forms.CharField(label="Last Name",max_length=100)
-    email = forms.EmailField(label="Email")
-    review = forms.CharField(label="Please Write Your Review Here!")
+
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label="First Name", max_length=100)
+#     last_name = forms.CharField(label="Last Name", max_length=100)
+#     email = forms.EmailField(label="Email")
+#     review = forms.CharField(
+#         label="Please Write Your Review Here!",
+#         widget=forms.Textarea(attrs={"class": "myform", "rows": "2", "cols": "2"}),
+#     )
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+        labels = {
+            "first_name": "YOUR FIRST NAME",
+            "last_name": "Last Name",
+            "stars": "Rating",
+        }
+        
+        error_messages = {
+            "stars": {
+                "min_value": "Min value is 1",
+                "max_value": "Max value is 5"
+            }
+        }
--- django_lecture/my_site_form/cars/views.py
+++ django_lecture/my_site_form/cars/views.py
@@ -4,15 +4,18 @@ from .forms import ReviewForm
 
 # Create your views here.
 def rental_review(request):
-    # POST REQUEST --> FORM CONTENTS --> THANK YOU
+    # POST 요청 --> 폼 --> THANK YOU
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
+            form.save()
             print(form.cleaned_data)
             return redirect(reverse("cars:thank_you"))
-        
-    # ELSE, RENDER FORM
+        else:
+            # 유효하지 않은 경우 원래 form 객체를 전달
+            return render(request, "cars/rental_review.html", context={"form": form})
     else:
+        # GET 요청의 경우 빈 폼을 전달
         return render(request, "cars/rental_review.html", context={"form": ReviewForm()})
 
 def thank_you(request):
