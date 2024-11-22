797f620a751e9c8172816f9f2cfbb17e27275eec
Yujin <yj999900@gmail.com>
2024-11-22 16:21:49 +0900

    .

--- react_lecture/voca/src/component/Hello.js
+++ react_lecture/voca/src/component/Hello.js
@@ -1,20 +1,7 @@
-import World from './World';
-import styles from './Hello.module.css'
-
 const Hello = function () {
   return (
     <div>
-      <h1
-        style={{
-          color: '#f00',
-          borderRight: '12px solid #000',
-          marginBottom: '30px',
-          opacity: 1,
-        }}
-      >
-        Hello
-      </h1>
-      <div className={styles.box}>Hello</div>
+      <h1>Hello</h1>
     </div>
   );
 };
